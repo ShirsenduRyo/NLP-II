9c345e5905039e6fb26744e6f88493a3f0c05581
ShirsenduRyo <sportshirsendu@gmail.com>
2023-07-13 23:14:47 +0530

    T5 First Draft

---
+++ Article Analyzer/ArticleAdvizer.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from transformers import T5Tokenizer, TFT5ForConditionalGeneration, T5Config
+
+class ArticleAdvizer:
+    def __init__(self, model_name="t5-base"):
+        self.tokenizer = T5Tokenizer.from_pretrained(model_name)
+        self.config = T5Config.from_pretrained(model_name)
+        self.model = TFT5ForConditionalGeneration.from_pretrained(model_name, \
+                                                                  config=self.config)
+        
+    def answer_question(self, news_article, question):
+        inputs = self.tokenizer.encode_plus(f"question: {question} \
+                    context: {news_article}", return_tensors="tf", padding="max_length", \
+                    truncation=True, max_length=512)
+        input_ids = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
+
+        outputs = self.model.generate(input_ids=input_ids, attention_mask=attention_mask)
+        answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return answer
+    
+    def summarize_article(self, news_article):
+        inputs = self.tokenizer.encode_plus(f"summarize: {news_article}", return_tensors="tf", \
+                                            padding="max_length", truncation=True, max_length=512)
+        input_ids = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
+
+        outputs = self.model.generate(input_ids=input_ids, attention_mask=attention_mask, \
+                                      max_length=150, min_length=40, length_penalty=2.0, num_beams=4)
+        summary = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return summary
